22522158f6736f62969443b01c7050195df4e391
Alexander Tampier <alexander.tampier@univie.ac.at>
2025-02-12 12:02:05 +0100

    minor deploy changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://luciendgolden.github.io',
+    base: '/atam-portfolio/',
     integrations: [
         mdx(),
         sitemap(),
