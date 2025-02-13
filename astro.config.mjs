ddf97723e32bade11c6107b63e8df4e21f0b39be
Alexander Tampier <alexander.tampier@univie.ac.at>
2025-02-13 12:04:36 +0100

    fix some base path rendering issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,32 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import rehypeRewrite from 'rehype-rewrite';
 
-// https://astro.build/config
+const BASE_URL = '/atam-portfolio';
+
 export default defineConfig({
     site: 'https://luciendgolden.github.io',
-    base: '/atam-portfolio',
+    base: BASE_URL,
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: true
         })
-    ]
+    ],
+    markdown: {
+        rehypePlugins: [
+            [
+                rehypeRewrite,
+                {
+                    rewrite: (node) => {
+                        if (node.tagName === 'img' && node.properties.src.startsWith('/')) {
+                            node.properties.src = `${BASE_URL}${node.properties.src}`;
+                        }
+                    }
+                }
+            ]
+        ]
+    }
 });
